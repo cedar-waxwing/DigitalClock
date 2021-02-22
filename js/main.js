3e3bfd32ffd4ec2851e9c06dedcb52e26a7e855c
Rachel Ehlers <rachel.ehlers1@gmail.com>
2021-02-21 22:37:25 -0500

    Fix for linking html document multiple times

--- js/main.js
+++ js/main.js
@@ -5,42 +5,42 @@ piece of code repeatedly at fixed time intervals.
 Its basic syntax is setInterval(function, milliseconds)."
 1000 msec is 1 sec. */
 
-setInterval(displayTime, 1000);
+
 
 //ClockDisplay -- stating variables. Uses Javascript Date object. new Date() defaults to display in milliseconds. 
 
 function displayTime() {
-    let date = new Date() 
+    let date = new Date()
     let hour = date.getHours();
     let mins = date.getMinutes();
     let secs = date.getSeconds();
     let period = "AM";
 
-/*Switching military to cilivian time as well as AM/PM differential; 
-can be included in same if statement setup.*/
- 
-if (hour < 12) {
-    period = "AM";
-}
-    if (hour>12) {
+    /*Switching military to cilivian time as well as AM/PM differential; 
+    can be included in same if statement setup.*/
+
+    if (hour < 12) {
+        period = "AM";
+    }
+    if (hour > 12) {
         hour -= 12;
         period = "PM";
     }
 
-//Ternary operator to abbreviate for loops. Example: "( condition ) ? run this code : otherwise run this code instead" 
+    /*Ternary operator to abbreviate if else statements. Example: "( condition ) ? run this code : otherwise run this code instead".
+     Here it's used to pad the clock with a 0 so that the clock has a consistant number of digits listed.
+    */
 
-hour = hour <10 ? "0" + hour : hour;
-mins = mins < 10 ? "0" + mins : mins;
-secs = secs < 10 ? "0" + secs : secs;
+    hour = hour < 10 ? "0" + hour : hour;
+    mins = mins < 10 ? "0" + mins : mins;
+    secs = secs < 10 ? "0" + secs : secs;
 
-//Making a string of hours, minutes and seconds.  
+    //Making a string of hours, minutes and seconds.  
 
-let currentTime = hour + ":" + mins + ":" + secs + period
+    let currentTime = hour + ":" + mins + ":" + secs + period
 
-//Referencing html to display the clock (https://www.w3schools.com/js/js_dates.asp)
+    //Referencing html to display the clock 
 
-document.getElementById("RachelsDigitalClock")
-    .innerHTML = currentTime
+    document.getElementById("RachelsDigitalClock")
+        .innerHTML = currentTime
 }
-
-displayTime()
